72f93df07986f1faa630812803054f0e4756a4d7
xiaowu zhang <-xiaowu.zhangv@gmail.com>
2014-05-21 17:23:32 +0200

    a test with renderjs

---
+++ component/js/progress.js
@@ -0,0 +1,28 @@
+/*jslint nomen: true*/
+/*global window, rJS */
+
+
+(function (window, rJS) {
+  "use strict";
+  
+  var gk = rJS(window);
+
+  gk.declareMethod('setValue', function (value) {
+    this.bar.value = value;
+  })
+    .declareMethod('setMax', function (max) {
+      this.bar.max = max;
+    })
+    .declareMethod('getValue', function () {
+      return this.bar.value;
+    })
+    .declareMethod('getMax', function () {
+      return this.bar.max;
+    });
+  gk.ready(function (g) {
+    g.bar = g.__element.getElementsByTagName('progress')[0];
+    g.bar.value = 10;
+    g.bar.max = 100;
+  });
+
+}(window, rJS));
